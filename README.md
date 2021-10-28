a46d001d81bfed23183865a934e3c21195043a95
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2021-10-28 15:43:27 +0900

    added RN chart kit, TS Utility Type for data form

--- README.md
+++ README.md
@@ -9,6 +9,18 @@ Apollo Client
 API     HKMA
 
 
-## Currency
+## Curreny and Time
 Currency abbreviations are [ISO 4217](https://en.wikipedia.org/wiki/ISO_4217) compliant.
+Date format is [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) compliant.
 
+## Technical Commentary
+<details>
+<ul>
+    <li>
+        First time using React Native. With limited screen area, UI design and prioritising utility is almost compulsory.
+    </li>
+    <li>
+        Installed Axios. Everytime I have to integrate third party API that I have no control of, I need to dig into the API documentation. But I am used to it after learning many languages and frameworks on my own.
+    </li>
+</ul>
+</details>
--- components/screens/ExchangeRates.tsx
+++ components/screens/ExchangeRates.tsx
@@ -1,48 +1,138 @@
 import React, { useState, useEffect} from "react";
-import { View, SafeAreaView, Text, FlatList, StyleSheet } from "react-native";
+import { View, SafeAreaView, Text, FlatList, StyleSheet, Dimensions } from "react-native";
 import { SafeAreaView as SAV} from "react-navigation";
+
+import { LineChart } from "react-native-chart-kit";
 
 import axios from "axios";
 
-const HKMA_API = "";
+const HKMA_API = "https://api.hkma.gov.hk/public/market-data-and-statistics/monthly-statistical-bulletin/er-ir/er-eeri-periodaverage";
 
 // this also defines the display order of exchange rates, total 14
-const currencies: string[] = ["usd", "eur", "gbp", "cny", "jpy", "cad", "aud",
-    "sgd", "krw", "twd", "chf", "myr", "php", "inr"];
+const currencies = ["usd", "eur", "gbp", "cny", "jpy", "cad", "aud",
+    "sgd", "krw", "twd", "chf", "myr", "php", "inr"] as const;      // read-only type-value
+type Currency = typeof currencies[number];      // "usd" | "eur" | ... | "inr"
+
+
+
 // you also need the end of month for graphing
 
-interface exchange_rate {
-    currency: string            // the name, e.g. USD
+interface ExchangeRate {
+    name: string
     rate: number[]
-    time: string[]
+    //time: string[]
 }
 
-interface exchange_rate_data {
-
+interface ExchangeRateData extends Record<Currency, ExchangeRate>{
 }
+
 
 const ExchangeRates = () => {
     // const { loading, error, data} = useQuery(getRates);
     // if (data) console.log(data);
-    const [ rates, setRates ] = useState([]);
+    const [ data, setData ] = useState<ExchangeRateData | undefined>(undefined);
 
+    // useEffect(() => {
+    //     axios.post("https://api.hkma.gov.hk/public/market-data-and-statistics/monthly-statistical-bulletin/er-ir/er-eeri-periodaverage",
+    //     {
+    //         offset: 100
+    //     }).then(response => {console.log(response.data.result.records)})
+    //     .catch(() => console.log("error"));
+    // }, []);
+    const today: Date = new Date();
+    console.log(today.toISOString());
+    const API_from = "2021-01"
+    //const API_to = today.toISOString()
+    const API_to = "2021-09"
     useEffect(() => {
+        axios.get(HKMA_API + `?from=${API_from}&to=${API_to}`)
+            .then(response => {
+                for (currency of currencies){
+                    
+                }
+            })
+            .catch(() => console.log("error"));
+
 
     }, []);
+    // axios.get(`https://api.hkma.gov.hk/public/market-data-and-statistics/monthly-statistical-bulletin/er-ir/er-eeri-periodaverage?
+    //     from=${API_from}&to=${API_to}`)
+    //     .then(response => {console.log(response.data.result.records)});
 
-    axios.get("https://api.hkma.gov.hk/public/market-data-and-statistics/monthly-statistical-bulletin/er-ir/er-eeri-periodaverage?offset=0")
-        .then(response => {console.log(response); console.log(response.data.result.records)})
-        //the records is an array of object
-        // object { "aud": , "bef": }
 
     return(
-        <>  {/* use FlatList here later */}
-            
+        <>
 
             <View>
             <Text>exchange rates</Text>
             </View>
-        </>
+
+            {/* test the effect of parameters of the chart below */}
+            
+            <View>
+                {/* <Text>Bezier Line Chart</Text> */}
+            <LineChart
+                data={{
+                    labels: ["Jan"],
+                    datasets: [{
+                        data: [1, 2, 3]
+                    }]
+                }}
+                width={Dimensions.get("window").width}
+                height={220}
+                chartConfig={{
+                    //backgroundColor: "#000000",
+                    backgroundGradientFrom: "#000000",
+                    backgroundGradientTo: "#000000",
+                    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                }}
+            />
+            <LineChart
+                data={{
+                labels: ["January", "February", "March", "April", "May", "June", "July", "Aug", "Sep"],
+                datasets: [
+                    {
+                    data: [
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100
+                    ]
+                    }
+                ]
+                }}
+                width={Dimensions.get("window").width} // from react-native
+                height={220}
+                yAxisLabel="$"
+                yAxisSuffix="k"
+                yAxisInterval={1} // optional, defaults to 1
+                chartConfig={{
+                    backgroundColor: "#e26a00",
+                    backgroundGradientFrom: "#fb8c00",
+                    backgroundGradientTo: "#ffa726",
+                    decimalPlaces: 2, // optional, defaults to 2dp
+                    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                    style: {
+                        borderRadius: 16
+                },
+                propsForDots: {
+                    r: "6",
+                    strokeWidth: "2",
+                    stroke: "#ffa726"
+                }
+                }}
+                bezier
+                style={{
+                    marginVertical: 8,
+                    borderRadius: 16
+                }}
+            />
+            </View>
+            </>
     );
 }
 
